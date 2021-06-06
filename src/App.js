bf7ce367bcbc64d64874f7ac564de4ed1a15d01f
== <==>
2021-06-07 00:09:07 +0300

    07.06.2021

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Dashboard from './layouts/Dashboard';
 import 'semantic-ui-css/semantic.min.css'
 import { Container } from 'semantic-ui-react';
-import Navi from './layouts/Navi';
+import Dashboard from './layouts/Dashboard';
+import Navi from './layouts/Navi'
 function App() {
   return (
     <div className="App">
--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -4,7 +4,7 @@ import CartSummary from "./CartSummary";
 export default function Navi() {
   return (
     <div>
-      <Menu inverted>
+      <Menu inverted fixed ="top">
         <Container>
           <Menu.Item name="home" />
           <Menu.Item name="messages" />
